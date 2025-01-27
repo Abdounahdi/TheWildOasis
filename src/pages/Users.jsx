72e2ce6fe9f01c3259fd343c279d92e6d0a72baf
Abdou <nahdi.abdo.2020@gmail.com>
2025-01-27 16:30:35 +0100

    learned about style components - set the routes for each page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,12 @@
 import Heading from "../ui/Heading";
 
-function NewUsers() {
+// function NewUsers() {
+//   return <Heading as="h1">Create a new user</Heading>;
+// }
+
+// export default NewUsers;
+function Users() {
   return <Heading as="h1">Create a new user</Heading>;
 }
 
-export default NewUsers;
+export default Users;
